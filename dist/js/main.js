111fc6ab60f90306477467b06a1cf982d7cb0a0f
rmleach <40783016+rmleach@users.noreply.github.com>
2020-02-28 18:37:23 -0700

    added project page

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,11 +5,14 @@ const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
+const bioSelectors = document.querySelector('.selectors')
+const bio = document.querySelector('.bio')
 
 //Set Initial State of Menu
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
+bioSelectors.addEventListener('click', switchBio);
 
 function toggleMenu() {
   if (!showMenu) {
@@ -33,3 +36,40 @@ function toggleMenu() {
 
   }
 }
+
+function switchBio(e) {
+  if (e.target.id === 'short') {
+    document.getElementById('p-short').style.display = "block"
+    document.getElementById('p-long').style.display = "none"
+    document.getElementById('p-speaker').style.display = "none"
+  }
+  if (e.target.id === 'long') {
+    document.getElementById('p-short').style.display = "none"
+    document.getElementById('p-long').style.display = "block"
+    document.getElementById('p-speaker').style.display = "none"
+  }
+  if (e.target.id === 'speaker') {
+    document.getElementById('p-short').style.display = "none"
+    document.getElementById('p-long').style.display = "none"
+    document.getElementById('p-speaker').style.display = "block"
+  }
+}
+
+  function toggleSelector(e) {
+    if (e.target.id === 'short') {
+      var p = document.createElement("P");
+      var ptext = document.createTextNode(short);
+      p.appendChild(ptext);
+      console.log(p, ptext)
+      bio.appendChild(p);
+    } else if (e.target.id === 'long') {
+      console.log('long')
+    } else if (e.target.id === 'speaker') {
+      console.log('speaker')
+    }
+
+
+  }
+
+  toggleMenu()
+  toggleSelector()
